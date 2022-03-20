170e50e17d80b5c694dce8b0cf0f3e1278b1f632
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-03-20 23:51:08 +0900

    scrollYprogress

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform } from "framer-motion";
-import { useRef } from "react";
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  useViewportScroll,
+} from "framer-motion";
+import { useEffect } from "react";
 const Wrapper = styled(motion.div)`
-  height: 100vh;
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -22,16 +27,17 @@ function App() {
   const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
   const gradient = useTransform(
     x,
-    [-800, 0, 800],
+    [-800, 800],
     [
       "linear-gradient(135deg, rgb(0,210,238), rgb(0,83,238))",
-      "linear-gradient(135deg, rgb(238,0,153), rgb(221,0,238))",
       "linear-gradient(135deg, rgb(0,238,155), rgb(238,178,0))",
     ]
   );
+  const { scrollYProgress } = useViewportScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
   return (
     <Wrapper style={{ background: gradient }}>
-      <Box style={{ x, rotateZ }} drag="x" dragSnapToOrigin />
+      <Box style={{ x, rotateZ, scale: scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
