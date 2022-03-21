9beeb4e848714b5843e04460e2709b0ee19f277f
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-03-22 01:25:31 +0900

    header setup 3:31

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
+import Header from "./Components/Header";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 export default App;
---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Nav = styled.nav``;
+
+const Col = styled.div``;
+
+function Header() {
+  return null;
+}
+
+export default Header;
